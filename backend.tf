83d4f7f0719819745778190f388002613960797e
Roei <joeygreg@DESKTOP-K0FVEMN.localdomain>
2025-12-23 18:10:51 -0500

    added missing variables file. corrected region in backend tf config.

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "tfstate-tavernlabs-03711579496081468369"
     key          = "okta/terraform.tfstate"
-    region       = "us-east-1"
+    region       = "us-east-2"
     encrypt      = true
     use_lockfile = true 
   }
